6c4cf3cd6c6f5b167821ff669b0851183667002f
samuel840923 <samuel840923@gmail.com>
2021-04-17 22:36:36 -0400

    route init page so that it doesnt call search directly

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,7 +5,6 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 project_name = "Best Food Finder"
 net_id = "April Ye yy459, Alan Huang ah2294, Geena Lee jl3257, Samuel Chen sc2992, Jack Ding jad493"
 
-
 def similarity_result(data, keyword):
 	'''
 	@data : dataframe with  pruned data
@@ -25,7 +24,7 @@ def similarity_result(data, keyword):
 	return data.iloc[ranked_i]
 
 
-@irsystem.route('/', methods=['GET'])
+@irsystem.route('/search', methods=['GET'])
 def search():
 	df = getdata()
 	print(df['price'][1])
@@ -49,3 +48,7 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+@irsystem.route('/', methods=['GET'])
+def home_page():
+	return render_template('search.html', name=project_name, netid=net_id)
